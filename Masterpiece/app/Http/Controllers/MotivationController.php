8e70e4e59ece216f67a35de5e2df40b90eb0a05c
Raghad Karasneh <raghad.karasneh@gmail.com>
2022-06-28 22:29:33 +0300

    Masterpiece

---
+++ Masterpiece/app/Http/Controllers/MotivationController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MotivationController extends Controller
+{
+    //
+}
--- Masterpiece/database/migrations/2014_10_12_000000_create_users_table.php
+++ Masterpiece/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,17 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            // $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('city');
+            $table->string('address');
+            $table->integer('phone_number');
+            $table->string('zip_code');
+            $table->boolean('is_deleted');
+            $table->string('is_admin');
             $table->rememberToken();
             $table->timestamps();
         });
